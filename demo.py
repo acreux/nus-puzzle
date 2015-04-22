35ef7f01fabb967a358c4634af58b6ac35a88eee
acreux <antoine.creux@gmail.com>
2015-04-23 03:20:10 +0800

    rename virgile.py to demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
-
 import string
 import math
+import random
 import itertools
 
-def signature(permut):
-    """Give the signature of the permutation
+def parity(permut):
+    """Give the parity of the permutation
     """
     permut = list(permut)
     b = sorted(permut)
@@ -15,29 +15,6 @@ def signature(permut):
         b.remove(first)
     return inversions % 2
 
-
-def check(p):
-    players = [i for i in string.ascii_lowercase][:p]
-
-    score = 0
-
-    for permutation in itertools.permutations(players, p):
-        # print order
-        tuples = [((permutation[i],) + permutation[:i] + permutation[i+1:]) for i in range(p)]
-        # print tuples
-        for ktuple, mtuple in zip(tuples, tuples[1:]):
-
-            if color_from_index(permutation_index(ktuple)) == color_from_index(permutation_index(mtuple)):
-                print permutation_index(ktuple), permutation_index(mtuple)
-                print "perdu"
-                continue
-            else:
-                score += 1
-    print score/(p-1)
-
-
-import random
-import itertools
 
 def generate_players(p=14, minin=0, maxim=10000):
     """
@@ -50,8 +27,8 @@ def generate_players(p=14, minin=0, maxim=10000):
     while len(numbers) < p:
         numbers.add(random.randint(minin, maxim))
 
-    return dict(list(enumerate(numbers)))
-
+    # Set is not oredered, and thus each player are assigned a random number.
+    return dict(enumerate(numbers))
 
 def check_result(color_list):
     """
@@ -88,9 +65,10 @@ def game(players_numbers):
 
         sorted_tuple_players = [i[0] for i in increase_players]
 
+        # Let s generate the view of our player
         tuple_p = (p, ) + tuple(sorted_tuple_players) 
 
-        return signature(tuple_p)  
+        return parity(tuple_p)  
 
     # Color associated to p
     colors = {}
@@ -105,22 +83,24 @@ def game(players_numbers):
     # Sorting by the game master
     items_players = [(k, v) for k, v in players_numbers.iteritems()]
     items_players.sort(key=lambda x: x[1])
-    # Is our result correct?
 
     return [colors[i[0]] for i in items_players]
 
 
-def check_many_games(p=14, n=10**7):
+def check_many_games(p=14, n=10000):
+    """Generate many games and check the results
+    """
     for i in range(n):
         players = generate_players(p)
         if not game(players):
             print "You lose"
             return
-        if not i%10000:
-            print i, " done"
-    print "Win"
+        if not i%1000:
+            print i, " checked"
+    print "{} games have been checked without errors.".format(n)
 
-check_many_games()
+if __name__ == "__main__":
+    check_many_games()
 
 
 
